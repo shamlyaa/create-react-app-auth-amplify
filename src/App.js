99207e422fe5a08de0f3cd582f17866969538c60
shamlyaa <shamlyaa@yahoo.com>
2023-05-08 20:54:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://www.virtualclinic.mobi/telecareus/frontend/site/login/"
+            href="https://virtualclinic.mobi/telecareus/frontend/site/login/"
             target="_blank"
             rel="noopener noreferrer"
           >
