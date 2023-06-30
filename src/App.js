600a11b2715970c9490a3c5fe805042275a531d4
shamlyaa <shamlyaa@yahoo.com>
2023-06-30 19:59:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -93,7 +93,6 @@ class App extends Component {
       <MyDoctor />
       <MyRpm />
       <MyMobile />
-      <MyAppointment />
        <MyRpmvideo />
         <header className="App-header">
         
