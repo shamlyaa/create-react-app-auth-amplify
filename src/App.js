410b1b9a085d52ac9fcd79b8c533f63f0623e92d
shamlyaa <shamlyaa@yahoo.com>
2023-07-14 11:14:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ class App extends Component {
         
           <p>
              TELECARE VIRTUAL CLINIC (TVC)
-      <script id='setmore_script' type='text/javascript' src='https://storage.googleapis.com/fullintegration-live/webComponentAppListing/Container/setmoreIframeLive.js'></script><a style='float:none;' id='Setmore_button_iframe' href='https://booking.setmore.com/scheduleappointment/71d408ea-6b28-40d9-adbd-7e5bce95378e'><img border='none' src='https://storage.googleapis.com/full-assets/setmore/images/1.0/Settings/book-now-blue.svg' alt='Click here to book the appointment using setmore' /></a>
+  <a href="https://telecare.scriptrapps.io/setmore.html">Book Now</a>
           </p>
     
       <h1>
